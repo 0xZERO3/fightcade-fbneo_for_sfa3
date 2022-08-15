d79ba8cfb4930af7bdf6dc08c0c47751bc0c4b85
0xZERO3 <0xzero3@gmail.com>
2022-08-15 22:02:59 +0900

    link error measures(bad)

--- src/intf/video/win32/vid_overlay.cpp
+++ src/intf/video/win32/vid_overlay.cpp
@@ -1338,8 +1338,8 @@ void VidOverlaySetSystemMessage(const wchar_t *text)
 	system_message.Set(text);
 }
 
-extern int nRollbackFrames;
-extern int nRollbackCount;
+int nRollbackFrames = 0;
+int nRollbackCount = 0;
 
 // jitter detector
 #define PINGSIZE 10
