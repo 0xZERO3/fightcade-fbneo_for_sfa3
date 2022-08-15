3e787e5f08d2726fc312e95f86b5d3b2bff9af45
0xZERO3 <0xzero3@gmail.com>
2022-08-15 22:01:28 +0900

    Revert "link error measures(bad)"
    This reverts commit 50a6f8bd3a8f64c2f699967a53519e04c9570e32.

--- src/intf/video/win32/vid_overlay.cpp
+++ src/intf/video/win32/vid_overlay.cpp
@@ -1338,8 +1338,8 @@ void VidOverlaySetSystemMessage(const wchar_t *text)
 	system_message.Set(text);
 }
 
-int nRollbackFrames;
-int nRollbackCount;
+extern int nRollbackFrames;
+extern int nRollbackCount;
 
 // jitter detector
 #define PINGSIZE 10
